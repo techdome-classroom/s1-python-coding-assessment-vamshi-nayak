51a9f91c325a40e0c75c7761f4615b81b92a6065
vamshi nayak <136154466+vamshi-nayak@users.noreply.github.com>
2024-11-11 13:52:09 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,6 +1,42 @@
+from collections import deque
+
 class Solution:
-   
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
+        if not grid or not grid[0]:
+            return 0
+
+        numRows = len(grid)
+        numCols = len(grid[0])
+        islandCount = 0
+
+        def bfs(startRow, startCol):
+            queue = deque([(startRow, startCol)])
+            grid[startRow][startCol] = 'W' 
+
+            directions = [
+                (0, 1),   
+                (1, 0),   
+                (0, -1),  
+                (-1, 0)   
+            ]
+
+            while queue:
+                currentRow, currentCol = queue.popleft()
+
+                for dRow, dCol in directions:
+                    newRow, newCol = currentRow + dRow, currentCol + dCol
+
                     
-        return 0
+                    if 0 <= newRow < numRows and 0 <= newCol < numCols and grid[newRow][newCol] == 'L':
+                        grid[newRow][newCol] = 'W' 
+                        queue.append((newRow, newCol))
+
+   
+        for row in range(numRows):
+            for col in range(numCols):
+                
+                if grid[row][col] == 'L':
+                    islandCount += 1
+                    bfs(row, col)
+
+        return islandCount

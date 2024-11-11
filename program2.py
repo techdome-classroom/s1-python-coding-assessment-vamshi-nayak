b9ea436c58e97c6e96fb00d21893971b8ee0fb3d
vamshi nayak <136154466+vamshi-nayak@users.noreply.github.com>
2024-11-11 13:54:35 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,25 @@
-def decode_message( s: str, p: str) -> bool:
+def decode_message(s: str, p: str) -> bool:
+    message_index = 0
+    pattern_index = 0
+    star_index = -1
+    match_index = 0
 
-# write your code here
-  
-        return False
+    while message_index < len(s):
+        if pattern_index < len(p) and (p[pattern_index] == s[message_index] or p[pattern_index] == '?'):
+            message_index += 1
+            pattern_index += 1
+        elif pattern_index < len(p) and p[pattern_index] == '*':
+            star_index = pattern_index
+            match_index = message_index
+            pattern_index += 1
+        elif star_index != -1:
+            pattern_index = star_index + 1
+            match_index += 1
+            message_index = match_index
+        else:
+            return False
+
+    while pattern_index < len(p) and p[pattern_index] == '*':
+        pattern_index += 1
+
+    return pattern_index == len(p)
